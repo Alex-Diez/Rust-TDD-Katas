fa98f7564be1c13a9cbbb1402a16b019584d2c3a
Alex-Diez <alexvic.amatory@gmail.com>
2015-11-23 11:21:25 +0200

    lexer kata day fourth

---
+++ lexer-sql-kata/day-4/src/tests.rs
@@ -0,0 +1,49 @@
+extern crate lexer;
+
+use lexer::Lexer;
+
+#[test]
+fn test_create_lexer() {
+    Lexer::new("some line here");
+}
+
+#[test]
+fn test_empty_line() {
+    let mut lexer = Lexer::new("");
+
+    assert_eq!(lexer.next_lexem(), None);
+}
+
+#[test]
+fn test_one_word_line_differ_case_and_underscore_plus_numbers() {
+    let mut lexer = Lexer::new("1_Word_Some_2_Where_3_Here");
+
+    assert_eq!(lexer.next_lexem(), Some("1_Word_Some_2_Where_3_Here".to_string()));
+    assert_eq!(lexer.next_lexem(), None);
+}
+
+#[test]
+fn test_words_devided_by_white_spaces() {
+    let mut lexer = Lexer::new("Word Some Where Here");
+
+    assert_eq!(lexer.next_lexem(), Some("Word".to_string()));
+    assert_eq!(lexer.next_lexem(), Some(" ".to_string()));
+    assert_eq!(lexer.next_lexem(), Some("Some".to_string()));
+    assert_eq!(lexer.next_lexem(), Some(" ".to_string()));
+    assert_eq!(lexer.next_lexem(), Some("Where".to_string()));
+    assert_eq!(lexer.next_lexem(), Some(" ".to_string()));
+    assert_eq!(lexer.next_lexem(), Some("Here".to_string()));
+    assert_eq!(lexer.next_lexem(), None);
+}
+
+#[test]
+fn test_sql_with_comment_line() {
+    let mut lexer = Lexer::new("Word //Some Comment Here\nAnother Word");
+
+    assert_eq!(lexer.next_lexem(), Some("Word".to_string()));
+    assert_eq!(lexer.next_lexem(), Some(" ".to_string()));
+    assert_eq!(lexer.next_lexem(), Some("Another".to_string()));
+    assert_eq!(lexer.next_lexem(), Some(" ".to_string()));
+    assert_eq!(lexer.next_lexem(), Some("Word".to_string()));
+    assert_eq!(lexer.next_lexem(), None);
+}
