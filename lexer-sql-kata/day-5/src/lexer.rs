38c7134a9dc2afbe39e710919bf17b66e3e64e3e
Alex-Diez <alexvic.amatory@gmail.com>
2015-11-24 16:01:50 +0000

    sql lexer kata fifth day

---
+++ lexer-sql-kata/day-5/src/lexer.rs
@@ -0,0 +1,72 @@
+#![feature(plugin)]
+#![plugin(regex_macros)]
+extern crate regex;
+
+use std::iter::Peekable;
+use std::str::Chars;
+
+use regex::Regex;
+
+#[derive(PartialEq, Debug)]
+pub enum Token {
+    Identifier(String),
+    Insert,
+    Into,
+    Values,
+    LeftParenthesis,
+    RightParenthesis,
+    SingleQuote,
+    Colon,
+    Constant(String),
+    Semicolon
+}
+
+pub struct Lexer<'a> {
+    iter: Peekable<Chars<'a>>,
+    insert: Regex,
+    into: Regex,
+    values: Regex
+}
+
+impl <'a> Lexer<'a> {
+
+    pub fn new(line: &'a str) -> Lexer {
+        Lexer {
+            iter: line.chars().peekable(),
+            insert: regex!("(?i)insert"),
+            into: regex!("(?i)into"),
+            values: regex!("(?i)values")
+        }
+    }
+
+    pub fn next_lexem(&mut self) -> Option<Token> {
+        let mut value = vec![];
+        loop {
+            let symbol = self.iter.peek().map(|c| *c);
+            match symbol {
+                Some(s @ 'a'...'z') | Some(s @ 'A'...'Z') |
+                Some(s @ '_') | Some(s @ '0'...'9') => { self.iter.next(); value.push(s) },
+                Some(_) | None => break,
+            }
+        }
+        println!("value size - {:?}", value.len());
+        if value.is_empty() {
+            None
+        }
+        else {
+            let string = value.iter().map(|c| *c).collect::<String>();
+            if self.insert.is_match(&string) {
+                Some(Token::Insert)
+            }
+            else if self.into.is_match(&string) {
+                Some(Token::Into)
+            }
+            else if self.values.is_match(&string) {
+                Some(Token::Values)
+            }
+            else {
+                Some(Token::Identifier(string))
+            }
+        }
+    }
+}
---
+++ lexer-sql-kata/day-5/src/tests.rs
@@ -0,0 +1,47 @@
+extern crate lexer;
+
+use lexer::Lexer;
+use lexer::Token;
+
+#[test]
+fn test_create_lexer() {
+    Lexer::new("some line here");
+}
+
+#[test]
+fn test_empty_lexem() {
+    let mut lexer = Lexer::new("");
+
+    assert_eq!(lexer.next_lexem(), None);
+}
+
+#[test]
+fn test_identifier_lexem() {
+    let mut lexer = Lexer::new("OnE_123");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Identifier("OnE_123".to_string())));
+    assert_eq!(lexer.next_lexem(), None);
+}
+
+#[test]
+fn test_insert_query() {
+    let mut lexer = Lexer::new("insert into tab1(col1, col2) values ('1', 2);");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Insert));
+    assert_eq!(lexer.next_lexem(), Some(Token::Into));
+    assert_eq!(lexer.next_lexem(), Some(Token::Identifier("tab1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Identifier("col1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::Identifier("col2".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Values));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::Constant("1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::Constant("2".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Semicolon));
+}
