b2c6ac010049e240fae9addbd34ab28552b1baa0
Alex-Diez <alexvic.amatory@gmail.com>
2015-11-27 15:55:39 +0000

    sql lexer kata eighth day

---
+++ lexer-sql-kata/day-8/src/lexer.rs
@@ -0,0 +1,132 @@
+use std::iter::Peekable;
+use std::str::Chars;
+use std::string::String;
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum Token {
+    Word(String),
+
+    //characters
+    LeftParenthesis,
+    RightParenthesis,
+    SemiColon,
+    SingleQuote,
+    Colon
+}
+
+pub struct Lexer <'a> {
+    iter: Peekable<Chars<'a>>,
+    previous: Option<Token>,
+    current_symbol: Option<char>,
+    previous_symbol: Option<char>
+}
+
+impl <'a> Lexer<'a> {
+    
+    pub fn new(line: &'a str) -> Lexer {
+        Lexer {
+            iter: line.chars().peekable(),
+            previous: None,
+            current_symbol: None,
+            previous_symbol: None,
+        }
+    }
+
+    fn skip_symbol(&mut self) {
+        self.iter.next();
+    }
+
+    fn populate_word(&mut self, word: &mut Vec<char>, s: char) {
+        self.skip_symbol();
+        word.push(s);
+    }
+
+    fn evaluate_word(&mut self, word: &mut Vec<char>) -> Option<Token> {
+        if word.is_empty() {
+            None
+        }
+        else {
+            let string = word.iter().map(|c| *c).collect::<String>().to_lowercase();
+            self.previous = Some(Token::Word(string.clone()));
+            Some(Token::Word(string))
+        }
+    }
+
+    fn do_token(&mut self, token: Token) -> Option<Token> {
+        self.skip_symbol();
+        self.previous = Some(token.clone());
+        Some(token)
+    }
+
+    pub fn next_lexem(&mut self) -> Option<Token> {
+        let mut value = vec![];
+        loop {
+            if self.previous_symbol != Some('\'') && self.current_symbol != Some('\'') {
+                self.previous_symbol = self.current_symbol;
+                self.current_symbol = self.iter.peek().map(|c| *c);
+            }
+            println!("68 previous - {:?} current - {:?}", self.previous_symbol, self.current_symbol);
+            match self.current_symbol {
+                Some(s @ 'a'...'z') | 
+                Some(s @ 'A'...'Z') |
+                Some(s @ '0'...'9') |
+                Some(s @ '_') => self.populate_word(&mut value, s),
+                Some('(') => return self.do_token(Token::LeftParenthesis),
+                Some(')') => {
+                    if !value.is_empty() && self.previous.is_some() {
+                        return self.evaluate_word(&mut value)
+                    }
+                    else {
+                        return self.do_token(Token::RightParenthesis)
+                    }
+                },
+                Some(';') => return self.do_token(Token::SemiColon),
+                Some('\'') => {
+                    
+                    if !value.is_empty() && self.previous.is_some() {
+                        self.previous_symbol = self.current_symbol;
+                        self.skip_symbol();
+                        self.current_symbol = self.iter.peek().map(|c| *c);
+                        match self.previous_symbol {
+                            Some('A'...'Z') |
+                            Some('a'...'z') |
+                            Some('0'...'9') |
+                            Some('_') => {
+                                println!("94 previous - {:?} current - {:?}", self.previous_symbol, self.current_symbol);
+                                if self.current_symbol != Some('\'') && self.previous_symbol == Some('\'') {
+                                    let c = self.previous_symbol.unwrap();
+                                    self.populate_word(&mut value, c)
+                                }
+                            },
+                            Some(' ') => return self.do_token(Token::SingleQuote),
+                            Some(_) | None => return self.evaluate_word(&mut value),
+                        }
+                    }
+                    else {
+                        return self.do_token(Token::SingleQuote)
+                    }
+                },
+                Some(',') => {
+                    if !value.is_empty() && self.previous.is_some() {
+                        return self.evaluate_word(&mut value)
+                    }
+                    else {
+                        return self.do_token(Token::Colon)
+                    }
+                },
+                Some(_) | None => {
+                    if !value.is_empty() {
+                        self.skip_symbol();
+                        return self.evaluate_word(&mut value)
+                    }
+                    else if self.previous.is_some() {
+                        self.skip_symbol();
+                    }
+                    else {
+                        return self.evaluate_word(&mut value)
+                    }
+                },
+            }
+        }
+    }
+}
---
+++ lexer-sql-kata/day-8/src/tests.rs
@@ -0,0 +1,138 @@
+extern crate lexer;
+
+use lexer::Lexer;
+use lexer::Token;
+
+#[test]
+fn test_create_lexer() {
+    Lexer::new("some line here");
+}
+
+#[test]
+fn test_empty_line() {
+    let mut lexer = Lexer::new("");
+
+    assert_eq!(lexer.next_lexem(), None);
+}
+
+#[test]
+fn test_insert_keyword() {
+    let mut lexer = Lexer::new("iNseRt");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("insert".to_string())));
+}
+
+#[test]
+fn test_into_keyword() {
+    let mut lexer = Lexer::new("into");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("into".to_string())));
+}
+
+#[test]
+fn test_identifier() {
+    let mut lexer = Lexer::new("ident");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("ident".to_string())));
+}
+
+#[test]
+fn test_values_keyword() {
+    let mut lexer = Lexer::new("values");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("values".to_string())));
+}
+
+#[test]
+fn test_left_parenthesis() {
+    let mut lexer = Lexer::new("(");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+}
+
+#[test]
+fn test_right_parenthesis() {
+    let mut lexer = Lexer::new(")");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+}
+
+#[test]
+fn test_semicolon() {
+    let mut lexer = Lexer::new(";");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::SemiColon));
+}
+
+#[test]
+fn test_single_quote() {
+    let mut lexer = Lexer::new("'");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+}
+
+#[test]
+fn test_insert_query() {
+    let mut lexer = Lexer::new("insert into tab1 values (1 , '1');");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("insert".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("into".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("tab1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("values".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::SemiColon));
+}
+
+#[test]
+fn test_insert_query_with_column_sequence() {
+    let mut lexer = Lexer::new("insert into tab1 (col_1 , col2) values (1, '1');");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("insert".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("into".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("tab1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("col_1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("col2".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("values".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::SemiColon));
+}
+
+#[test]
+#[ignore]
+fn test_escaping_by_double_single_qout() {
+    let mut lexer = Lexer::new("insert into tab1 (col_1 , col2 ) values (1, 'ab''s');");
+
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("insert".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("into".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("tab1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("col_1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("col2".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("values".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::LeftParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("1".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::Colon));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::Word("ab's".to_string())));
+    assert_eq!(lexer.next_lexem(), Some(Token::SingleQuote));
+    assert_eq!(lexer.next_lexem(), Some(Token::RightParenthesis));
+    assert_eq!(lexer.next_lexem(), Some(Token::SemiColon));
+    
+}
