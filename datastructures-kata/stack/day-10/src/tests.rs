4d435988941a7261264df1176e1a497b5e0e3f1e
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-12 11:04:04 +0000

    stack kata tenth day

---
+++ datastructures-kata/stack/day-10/src/tests.rs
@@ -0,0 +1,58 @@
+#![feature(const_fn,plugin)]
+#![plugin(stainless)]
+
+extern crate collections;
+
+pub use collections::Stack;
+
+describe! stack_tests {
+
+    before_each {
+        let mut stack: Stack<i32> = Stack::new(20);
+    }
+
+    it "should create a new empty stack" {
+        assert_eq!(stack.size(), 0);
+        assert!(stack.is_empty());
+    }
+
+    it "should increase stack size when push into it" {
+        let old_size = stack.size();
+        stack.push(10);
+
+        assert_eq!(stack.size(), old_size + 1);
+        assert!(!stack.is_empty());
+    }
+
+    it "should decrease stack size when pop from it" {
+        stack.push(20);
+        let old_size = stack.size();
+        stack.pop();
+
+        assert_eq!(stack.size(), old_size - 1);
+    }
+
+    it "should pop value that was pushed into the stack" {
+        stack.push(20);
+
+        assert_eq!(stack.pop(), Some(20));
+
+        stack.push(10);
+
+        assert_eq!(stack.pop(), Some(10));
+    }
+
+    it "should pop value last that was pushed first into the stack" {
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        assert_eq!(stack.pop(), Some(50));
+        assert_eq!(stack.pop(), Some(40));
+        assert_eq!(stack.pop(), Some(30));
+        assert_eq!(stack.pop(), Some(20));
+        assert_eq!(stack.pop(), Some(10));
+    }
+}
