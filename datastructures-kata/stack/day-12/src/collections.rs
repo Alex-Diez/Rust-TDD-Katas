cc5eb265e017b956126425916b24f3dd7f2a611d
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-14 07:04:02 +0000

    stack kata twelfth day

---
+++ datastructures-kata/stack/day-12/src/collections.rs
@@ -0,0 +1,74 @@
+#![feature(alloc,core_intrinsics)]
+
+extern crate alloc;
+
+use alloc::raw_vec::RawVec;
+
+use std::ops::{Deref, DerefMut};
+use std::{slice, ptr};
+use std::intrinsics::assume;
+
+pub struct Stack<T> {
+    size: usize,
+    raw_vec: RawVec<T>
+}
+
+impl <T> Deref for Stack<T> {
+    type Target = [T];
+
+    fn deref(&self) -> &[T] {
+        unsafe {
+            let ptr = self.raw_vec.ptr();
+            assume(!ptr.is_null());
+            slice::from_raw_parts(ptr, self.size)
+        }
+    }
+}
+
+impl <T> DerefMut for Stack<T> {
+    
+    fn deref_mut(&mut self) -> &mut [T] {
+        unsafe {
+            let ptr = self.raw_vec.ptr();
+            assume(!ptr.is_null());
+            slice::from_raw_parts_mut(ptr, self.size)
+        }
+    }
+}
+
+impl <T> Stack<T> {
+    
+    pub fn new(max_size: usize) -> Stack<T> {
+        Stack { size: 0, raw_vec: RawVec::with_capacity(max_size) }
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+
+    pub fn push(&mut self, val: T) {
+        if self.size < self.raw_vec.cap() {
+            unsafe {
+                let end = self.as_mut_ptr().offset(self.size as isize);
+                ptr::write(end, val);
+            }
+            self.size += 1;
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if !self.is_empty() {
+            self.size -= 1;
+            unsafe {
+                Some(ptr::read(self.get_unchecked(self.size)))
+            }
+        }
+        else {
+            None
+        }
+    }
+}
---
+++ datastructures-kata/stack/day-12/src/tests.rs
@@ -0,0 +1,58 @@
+#![feature(const_fn, plugin)]
+#![plugin(stainless)]
+
+extern crate collections;
+
+pub use collections::Stack;
+
+describe! stack_tests {
+
+    before_each {
+        let mut stack: Stack<i32> = Stack::new(20);
+    }
+
+    it "should create a new empty stack" {
+        assert_eq!(stack.size(), 0);
+        assert!(stack.is_empty());
+    }
+
+    it "should increase stack size when push into it" {
+        let old_size = stack.size();
+        stack.push(10);
+
+        assert_eq!(old_size + 1, stack.size());
+        assert!(!stack.is_empty());
+    }
+
+    it "should decrease stack size when pop from it" {
+        stack.push(20);
+        let old_size = stack.size();
+        stack.pop();
+
+        assert_eq!(old_size - 1, stack.size());
+    }
+
+    it "should pop value that was pushed into the stack" {
+        stack.push(10);
+
+        assert_eq!(stack.pop(), Some(10));
+
+        stack.push(20);
+
+        assert_eq!(stack.pop(), Some(20));
+    }
+
+    it "should pop last pushed first value into the stack" {
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        assert_eq!(stack.pop(), Some(50));
+        assert_eq!(stack.pop(), Some(40));
+        assert_eq!(stack.pop(), Some(30));
+        assert_eq!(stack.pop(), Some(20));
+        assert_eq!(stack.pop(), Some(10));
+    }
+}
