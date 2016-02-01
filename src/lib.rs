9343fad3c4deee0949479da57fab63da0c59c450
Alex-Diez <alexvic.amatory@gmail.com>
2016-02-01 11:22:22 +0000

    move bowling kata

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@
 
 extern crate regex;
 
+pub mod bowling_kata;
 pub mod string_calc_kata;
 pub mod lexer_sql_kata;
--- test/lib.rs
+++ test/lib.rs
@@ -2,5 +2,6 @@
 #![plugin(stainless)]
 extern crate tdd_kata;
 
+mod bowling_kata;
 mod string_calc_kata;
 mod lexer_sql_kata;
