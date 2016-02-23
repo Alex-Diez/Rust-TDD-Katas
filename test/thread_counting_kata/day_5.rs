8b1476fb211f979332703e17c2ff2de5c5bf1ff2
Alex-Diez <alexvic.amatory@gmail.com>
2016-02-23 09:00:58 +0200

    thread counting kata fifth day

---
+++ test/thread_counting_kata/day_5.rs
@@ -0,0 +1,79 @@
+pub use std::collections::HashSet;
+
+pub use tdd_kata::thread_counting_kata::day_5::{ThreadCounter, Counter};
+
+pub use expectest::prelude::be_equal_to;
+
+describe! thread_counting_tests {
+
+    it "should create a new thread counter" {
+        ThreadCounter::new(3, 100);
+    }
+
+    it "should count from 1 till 100" {
+        let thread_counter = ThreadCounter::new(3, 100);
+        let counter = Counter::new();
+
+        thread_counter.count_in_threads(&counter);
+
+        let data = (1..).take(100).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers()).to(be_equal_to(data));
+    }
+
+    it "should count from 1 till 200" {
+        let thread_counter = ThreadCounter::new(3, 200);
+        let counter = Counter::new();
+
+        thread_counter.count_in_threads(&counter);
+
+        let data = (1..).take(200).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers()).to(be_equal_to(data));
+    }
+
+    it "should count in 3 threads" {
+        let thread_counter = ThreadCounter::new(3, 100);
+        let counter = Counter::new();
+
+        thread_counter.count_in_threads(&counter);
+
+        expect!(counter.threads()).to(be_equal_to(3));
+    }
+
+    it "should count in 5 threads" {
+        let thread_counter = ThreadCounter::new(5, 100);
+        let counter = Counter::new();
+
+        thread_counter.count_in_threads(&counter);
+
+        expect!(counter.threads()).to(be_equal_to(5));
+    }
+
+    it "should count its own numbers set" {
+        let thread_counter = ThreadCounter::new(5, 100);
+        let counter = Counter::new();
+
+        thread_counter.count_in_threads(&counter);
+
+        let thread_data = (1..).take(100).filter(|i| i % 5 == 1).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers_of("Thread-1")).to(be_equal_to(thread_data));
+
+        let thread_data = (1..).take(100).filter(|i| i % 5 == 2).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers_of("Thread-2")).to(be_equal_to(thread_data));
+
+        let thread_data = (1..).take(100).filter(|i| i % 5 == 3).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers_of("Thread-3")).to(be_equal_to(thread_data));
+
+        let thread_data = (1..).take(100).filter(|i| i % 5 == 4).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers_of("Thread-4")).to(be_equal_to(thread_data));
+
+        let thread_data = (1..).take(100).filter(|i| i % 5 == 0).collect::<HashSet<i32>>();
+
+        expect!(counter.numbers_of("Thread-5")).to(be_equal_to(thread_data));
+    }
+}
