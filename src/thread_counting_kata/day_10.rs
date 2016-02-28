0b61bbbfa2f94d7c9a3a2d8e3314c45f6d475721
Alex-Diez <alexvic.amatory@gmail.com>
2016-02-28 08:58:58 +0200

    thread counting kata tenth day

---
+++ src/thread_counting_kata/day_10.rs
@@ -0,0 +1,120 @@
+use std::collections::{HashSet, HashMap};
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
+
+struct CounterIneer {
+    numbers: Vec<i32>,
+    threads: HashMap<Option<String>, Vec<i32>>
+}
+
+impl CounterIneer {
+
+    fn new() -> CounterIneer {
+        CounterIneer {
+            numbers: Vec::new(),
+            threads: HashMap::new()
+        }
+    }
+
+    fn numbers(&self) -> HashSet<i32> {
+        self.numbers.iter().cloned().collect::<HashSet<i32>>()
+    }
+
+    fn threads(&self) -> usize {
+        self.threads.len()
+    }
+
+    fn numbers_of(&self, name: &str) -> HashSet<i32> {
+        self.threads[&Some(name.to_owned())].iter().cloned().collect::<HashSet<i32>>()
+    }
+
+    fn count(&mut self, n: i32) {
+        let thread_name = thread::current().name().map(|n| n.to_owned());
+        self.threads.entry(thread_name).or_insert_with(Vec::new).push(n);
+        self.numbers.push(n);
+    }
+}
+
+#[derive(Clone)]
+pub struct Counter {
+    inner: Arc<Mutex<CounterIneer>>
+}
+
+impl Counter {
+
+    pub fn new() -> Counter {
+        Counter {
+            inner: Arc::new(Mutex::new(CounterIneer::new()))
+        }
+    }
+
+    pub fn numbers(&self) -> HashSet<i32> {
+        let guard = self.inner.lock().unwrap();
+        guard.numbers()
+    }
+
+    pub fn threads(&self) -> usize {
+        let guard = self.inner.lock().unwrap();
+        guard.threads()
+    }
+
+    pub fn numbers_of(&self, name: &str) -> HashSet<i32> {
+        let guard = self.inner.lock().unwrap();
+        guard.numbers_of(name)
+    }
+
+    fn count(&self, n: i32) {
+        let mut guard = self.inner.lock().unwrap();
+        guard.count(n);
+    }
+}
+
+pub struct ThreadCounter {
+    number_of_threads: usize,
+    limit: usize
+}
+
+impl ThreadCounter {
+
+    pub fn new(number_of_threads: usize, limit: usize) -> ThreadCounter {
+        ThreadCounter {
+            number_of_threads: number_of_threads,
+            limit: limit
+        }
+    }
+
+    pub fn count_in_threads(&self, counter: &Counter) {
+        let mut flags = Vec::with_capacity(self.number_of_threads);
+        for _ in 0..self.number_of_threads {
+            flags.push(Arc::new(AtomicBool::new(false)));
+        }
+        flags[0].store(true, Ordering::SeqCst);
+        let mut joins = Vec::with_capacity(self.number_of_threads);
+        for i in (1..).take(self.number_of_threads) {
+            let limit = self.limit as i32;
+            let counter = counter.clone();
+            let increment = self.number_of_threads as i32;
+            let start = i as i32;
+            let allow_flag = flags[(i-1) % self.number_of_threads].clone();
+            let ready_flag = flags[i % self.number_of_threads].clone();
+            let join = thread::Builder::new().name(format!("Thread-{}", i)).spawn(
+                move || {
+                    let mut current = start;
+                    while current <= limit {
+                        while !allow_flag.load(Ordering::SeqCst) {
+                        }
+                        allow_flag.store(false, Ordering::SeqCst);
+                        counter.count(current);
+                        ready_flag.store(true, Ordering::SeqCst);
+                        current += increment;
+                    }
+                }
+            ).unwrap();
+            joins.push(join);
+        }
+        for j in joins {
+            drop(j.join());
+        }
+    }
+}
