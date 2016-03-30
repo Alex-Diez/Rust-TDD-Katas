25f02e1566d3aba9ef0a3afe75a6012611aaf7fa
Alex-Diez <alexvic.amatory@gmail.com>
2016-03-30 09:04:33 +0300

    depth first search fourth day

---
+++ src/dfs_kata/day_4.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+
+pub struct UnidirecredGraph {
+    vertices: HashMap<isize, Vec<isize>>,
+    edges: usize
+}
+
+impl Default for UnidirecredGraph {
+
+    fn default() -> UnidirecredGraph {
+        UnidirecredGraph {
+            vertices: HashMap::new(),
+            edges: 0
+        }
+    }
+}
+
+impl UnidirecredGraph {
+
+    pub fn edges(&self) -> usize {
+        self.edges
+    }
+
+    pub fn vertices(&self) -> usize {
+        self.vertices.len()
+    }
+
+    pub fn add_edge(&mut self, v: isize, w: isize) {
+        self.edges += 1;
+        self.vertices.entry(v).or_insert_with(Vec::new).push(w);
+        self.vertices.entry(w).or_insert_with(Vec::new).push(v);
+    }
+
+    pub fn adjacent_to(&self, v: isize) -> Option<&Vec<isize>> {
+        self.vertices.get(&v)
+    }
+}
+
+pub struct DepthFirstSearch {
+    marked: HashMap<isize, bool>
+}
+
+impl DepthFirstSearch {
+
+    pub fn new(graph: &UnidirecredGraph, src: isize) -> DepthFirstSearch {
+        let mut dfs = DepthFirstSearch {
+            marked: HashMap::default()
+        };
+        if graph.vertices() > 0 {
+            dfs.search(graph, src);
+        }
+        dfs
+    }
+
+    fn search(&mut self, graph: &UnidirecredGraph, v: isize) {
+        self.marked.insert(v, true);
+        if let Some(adj) = graph.adjacent_to(v) {
+            for w in adj {
+                if !(*(self.marked.entry(*w).or_insert(false))) {
+                    self.search(&graph, *w);
+                }
+            }
+        }
+    }
+
+    pub fn has_path(&self, v: isize) -> bool {
+        self.marked.contains_key(&v)
+    }
+}
---
+++ test/dfs_kata/day_4.rs
@@ -0,0 +1,68 @@
+pub use tdd_kata::dfs_kata::day_4::{UnidirecredGraph, DepthFirstSearch};
+
+pub use expectest::prelude::{be_equal_to, be_some, be_true, be_false};
+
+describe! dfs {
+
+    describe! graph {
+
+        before_each {
+            let mut graph = UnidirecredGraph::default();
+        }
+
+        it "should create a new empty graph" {
+            expect!(graph.edges()).to(be_equal_to(0));
+            expect!(graph.vertices()).to(be_equal_to(0));
+        }
+
+        it "should add edge to a graph" {
+            graph.add_edge(1, 2);
+
+            expect!(graph.edges()).to(be_equal_to(1));
+            expect!(graph.vertices()).to(be_equal_to(2));
+        }
+
+        it "should add edges to a graph" {
+            graph.add_edge(1, 2);
+            graph.add_edge(1, 3);
+            graph.add_edge(2, 4);
+
+            expect!(graph.vertices()).to(be_equal_to(4));
+            expect!(graph.edges()).to(be_equal_to(3));
+        }
+
+        it "should be adjacent to each other" {
+            graph.add_edge(1, 2);
+
+            expect!(graph.adjacent_to(1)).to(be_some().value(&vec![2]));
+            expect!(graph.adjacent_to(2)).to(be_some().value(&vec![1]));
+        }
+    }
+
+    describe! search {
+
+        it "should create a depth first search" {
+            let graph = UnidirecredGraph::default();
+            DepthFirstSearch::new(&graph, 1);
+        }
+
+        it "should have path to transetive vertices" {
+            let mut graph = UnidirecredGraph::default();
+            graph.add_edge(1, 2);
+            graph.add_edge(2, 3);
+            let search = DepthFirstSearch::new(&graph, 1);
+
+            expect!(search.has_path(2)).to(be_true());
+        }
+
+        it "shold not have path to not connected vertices" {
+            let mut graph = UnidirecredGraph::default();
+            graph.add_edge(1, 2);
+            graph.add_edge(3, 4);
+            let search = DepthFirstSearch::new(&graph, 1);
+
+            expect!(search.has_path(3)).to(be_false());
+        }
+    }
+
+}
