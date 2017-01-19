8aebdfcee0f5f73a7ec3380e2cf3bdb525e52322
Alex-Diez <alexvic.amatory@gmail.com>
2017-01-19 22:16:13 +0200

    linked list stack kata ninth day

---
+++ linked_list_stack_kata/src/day_9.rs
@@ -0,0 +1,78 @@
+use std::mem;
+
+struct Node<E> {
+    elem: E,
+    next: Link<E>
+}
+
+enum Link<E> {
+    Empty,
+    Cons(Box<Node<E>>)
+}
+
+pub struct Stack<E> {
+    head: Link<E>
+}
+
+impl <E> Stack<E> {
+
+    pub fn new() -> Self {
+        Stack { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: E) {
+        let node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty)
+        });
+
+        self.head = Link::Cons(node);
+    }
+
+    pub fn pop(&mut self) -> Option<E> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::Cons(boxed) => {
+                let node = *boxed;
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_an_empty_stack() {
+        let mut stack: Stack<i32> = Stack::new();
+
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn adds_an_element_to_a_stack() {
+        let mut stack = Stack::new();
+
+        stack.push(1);
+
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn adds_elements_to_a_stack() {
+        let mut stack = Stack::new();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        assert_eq!(stack.pop(), Some(30));
+        assert_eq!(stack.pop(), Some(20));
+        assert_eq!(stack.pop(), Some(10));
+        assert_eq!(stack.pop(), None);
+    }
+}
