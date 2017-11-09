73ec655d278f4e953b1a2b0eb59b0a37c63e00ee
Alex-Diez <alex.dukhno@icloud.com>
2017-11-09 18:15:07 +0200

    linked stack kata third day

---
+++ linked_stack_kata/src/day_3.rs
@@ -0,0 +1,183 @@
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    item: T,
+    next: Link<T>
+}
+
+impl <T> Node<T> {
+    fn new(item: T, next: Link<T>) -> Self {
+        Node { item, next }
+    }
+}
+
+pub struct Stack<T> {
+    head: Link<T>
+}
+
+impl <T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { head: None }
+    }
+
+    pub fn push(&mut self, item: T) {
+        let old_head = self.head.take();
+        self.head = Some(Box::new(Node::new(item, old_head)));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.item
+        })
+    }
+}
+
+impl <T> IntoIterator for Stack<T> {
+    type Item = T;
+    type IntoIter = StackIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        StackIter { stack: self }
+    }
+}
+
+pub struct StackIter<T> {
+    stack: Stack<T>
+}
+
+impl <T> Iterator for StackIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
+
+impl <T> AsRef<Self> for Stack<T> {
+    fn as_ref(&self) -> &Self {
+        self
+    }
+}
+
+impl<'s, T: 's> IntoIterator for &'s Stack<T> {
+    type Item = &'s T;
+    type IntoIter = StackIterRef<'s, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        StackIterRef { node: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+pub struct StackIterRef<'s, T: 's> {
+    node: Option<&'s Node<T>>
+}
+
+impl<'s, T: 's> Iterator for StackIterRef<'s, T> {
+    type Item = &'s T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.node.take().map(|node| {
+            self.node = node.next.as_ref().map(|node| &**node);
+            &node.item
+        })
+    }
+}
+
+impl <T> AsMut<Self> for Stack<T> {
+    fn as_mut(&mut self) -> &mut Self {
+        self
+    }
+}
+
+impl<'s, T: 's> IntoIterator for &'s mut Stack<T> {
+    type Item = &'s mut T;
+    type IntoIter = StackIterRefMut<'s, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        StackIterRefMut { node: self.head.as_mut().map(|node| &mut **node) }
+    }
+}
+
+pub struct StackIterRefMut<'s, T: 's> {
+    node: Option<&'s mut Node<T>>
+}
+
+impl<'s, T: 's> Iterator for StackIterRefMut<'s, T> {
+    type Item = &'s mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.node.take().map(|node| {
+            self.node = node.next.as_mut().map(|node| &mut **node);
+            &mut node.item
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push_many_pop_all_items() {
+        let mut stack = Stack::new();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn iterate_over_stack_move() {
+        let mut stack = Stack::new();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        let mut iter = stack.into_iter();
+
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iterate_over_stack_borrow() {
+        let mut stack = Stack::new();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        let mut iter = stack.as_ref().into_iter();
+
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iterate_over_stack_borrow_mut() {
+        let mut stack = Stack::new();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        let mut iter = stack.as_mut().into_iter();
+
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next(), None);
+    }
+}
