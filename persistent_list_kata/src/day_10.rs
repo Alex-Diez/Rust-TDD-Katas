4840bce9420ed32df81325b555ed32308b8ebfdb
Alex Dukhno <alex.dukhno@icloud.com>
2020-03-18 18:56:47 +0200

    persistent list tenth day

--- persistent_list_kata/src/day_10.rs
+++ persistent_list_kata/src/day_10.rs
@@ -1,57 +1,49 @@
 use std::rc::Rc;
 
-type Link<T> = Option<Rc<Node<T>>>;
-
-struct Node<T> {
-    item: T,
-    next: Link<T>,
+struct Node {
+    item: i32,
+    next: Option<Rc<Node>>
 }
 
-impl<T> Node<T> {
-    fn new(item: T, next: Link<T>) -> Link<T> {
-        Some(Rc::new(Node { item, next }))
-    }
+pub struct List {
+    head: Option<Rc<Node>>
 }
 
-pub struct List<T> {
-    head: Link<T>
-}
-
-impl<T> List<T> {
-    pub fn head(&self) -> Option<&T> {
+impl List {
+    pub fn head(&self) -> Option<&i32> {
         self.head.as_ref().map(|node| &node.item)
     }
 
-    pub fn append(&self, item: T) -> Self {
-        Self { head: Node::new(item, self.head.clone()) }
+    pub fn append(&self, item: i32) -> Self {
+        Self { head: Some(Rc::new(Node{item, next: self.head.clone()}))}
     }
 
     pub fn tail(&self) -> Self {
-        Self { head: self.head.as_ref().and_then(|node| node.next.clone()) }
+        Self { head: self.head.as_ref().and_then(|node| node.next.clone())}
     }
 }
 
-impl<T> Default for List<T> {
+impl Default for List {
     fn default() -> Self {
         Self { head: None }
     }
 }
 
-impl<'l, T> IntoIterator for &'l List<T> {
-    type Item = &'l T;
-    type IntoIter = ListIter<'l, T>;
+impl<'l> IntoIterator for &'l List {
+    type Item = &'l i32;
+    type IntoIter = ListIter<'l>;
 
     fn into_iter(self) -> Self::IntoIter {
         ListIter { node: self.head.as_ref().map(|node| &**node)}
     }
 }
 
-pub struct ListIter<'l, T> {
-    node: Option<&'l Node<T>>
+pub struct ListIter<'l> {
+    node: Option<&'l Node>
 }
 
-impl<'l, T> Iterator for ListIter<'l, T> {
-    type Item = &'l T;
+impl<'l> Iterator for ListIter<'l> {
+    type Item = &'l i32;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.node.take().map(|node| {
@@ -61,7 +53,7 @@ impl<'l, T> Iterator for ListIter<'l, T> {
     }
 }
 
-impl<T> Drop for List<T> {
+impl Drop for List {
     fn drop(&mut self) {
         let mut link = self.head.take();
 
@@ -77,7 +69,7 @@ mod tests {
 
     #[test]
     fn empty_list_head() {
-        let list: List<i32> = List::default();
+        let list = List::default();
 
         assert_eq!(list.head(), None);
     }
@@ -103,6 +95,22 @@ mod tests {
         let list = List::default().append(1).append(2).append(3);
 
         let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn middle_list_drop() {
+        let middle_list = List::default().append(1).append(2);
+
+        let list = middle_list.append(3);
+
+        let mut iter = list.into_iter();
+
+        drop(middle_list);
 
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
